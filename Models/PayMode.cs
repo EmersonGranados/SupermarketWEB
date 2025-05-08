73521c80123d37d3001cf7c2173c00edfe74caf1
Emerson <emerson@gmail.com>
2025-05-07 22:22:43 -0500

    Create Product

--- Models/PayMode.cs
+++ Models/PayMode.cs
@@ -2,5 +2,8 @@
 {
     public class PayMode
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Observation { get; set; }
     }
 }
--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Products
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+        public IndexModel(SupermarketContext context) => _context = context;
+
+        public IList<Product> Products { get; set; } = default!;
+
+        public async Task OnGetAsync()
         {
+            if (_context.Products != null)
+            {
+                Products = await _context.Products
+                    .Include(p => p.Category) // Carga la categoría relacionada
+                    .ToListAsync();
+            }
         }
     }
 }
